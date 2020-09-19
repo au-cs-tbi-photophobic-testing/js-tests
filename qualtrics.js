7ef53ea906a58337c57e33dd0ccb6619cdb03220
Justice Suh <justice.suh@gmail.com>
2020-09-18 22:05:48 -0400

    Emulate survey engine

---
+++ qualtrics.js
@@ -0,0 +1,35 @@
+class SurveyEngine {
+    addOnload(callback) {
+        window.onload = callback;
+    }
+
+    addOnReady(callback) {
+        if (document.readyState !== 'loading') {
+            callback();
+        } else if (document.addEventListener) {
+            document.addEventListener('DOMContentLoaded', callback);
+        } else {
+            document.attachEvent('onreadystatechanged', function() {
+                if (document.readyState === 'complete') {
+                    callback();
+                }
+            });
+        }
+    }
+
+    addOnUnload(callback) {
+        window.onunload = callback;
+    }
+
+    setEmbeddedData(data) {
+        
+    }
+}
+
+class Wrapper {
+    constructor() {
+        this.SurveyEngine = new SurveyEngine();
+    }
+}
+
+const Qualtrics = new Wrapper();
